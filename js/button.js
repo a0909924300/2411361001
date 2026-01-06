a2c3de7562486a6cb75df27095ef05985d7f55af
Tony <a0909924300@gmail.com>
2026-01-06 12:26:20 +0800

    Update button.js

--- js/button.js
+++ js/button.js
@@ -1,6 +1,7 @@
-// 彈出訊息按鈕功能
+<!-- 按鈕內容 -->
 const alertBtn = document.getElementById("alertBtn");
 
 alertBtn.addEventListener("click", function() {
     alert("歡迎來到我的自我介紹網站！希望你會喜歡這個網站，並且能更加認識我 😊");
 });
+
