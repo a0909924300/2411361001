adb57c6df2e26905a80e0b8745551d69b5a08772
Tony <a0909924300@gmail.com>
2026-01-06 12:45:29 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,2 @@
 <!-- 按鈕內容 -->
-document.addEventListener("DOMContentLoaded", function() {
-    const btn = document.getElementById("welcomeBtn");
-    if(btn){
-        btn.addEventListener("click", function(){
-            alert("謝謝您造訪我們的網站!再麻煩您幫我們的粉絲專業按個追蹤以及按個讚，多多支持一下我們!!!");
-        });
-    }
-});
-
+<button onclick="alert('謝謝您造訪我們的網站!再麻煩您幫我們的粉絲專業按個追蹤以及按個讚，多多支持一下我們!!!')">點我</button>
