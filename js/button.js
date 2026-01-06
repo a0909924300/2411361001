ea781059ead7dbf1c7b5ba919eb6f12a42531683
Tony <a0909924300@gmail.com>
2026-01-06 15:32:51 +0800

    Delete js directory

--- js/button.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const btn = document.getElementById("welcomeBtn");
-    if(btn){
-        btn.addEventListener("click", function(){
-            alert("歡迎來到我的自我介紹網站！希望你會喜歡這個網站，並且能更加認識我 😊");
-        });
-    }
-});
