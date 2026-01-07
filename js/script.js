66e577974287344696350fda58cfdd815fe4757e
Tony <a0909924300@gmail.com>
2026-01-07 13:03:22 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const btn = document.getElementById("welcomeBtn");
+    if(btn){
+        btn.addEventListener("click", function(){
+            alert("謝謝您造訪我們的網站!再麻煩您幫我們的粉絲專業按個追蹤以及按個讚，多多支持一下我們!!!");
+        });
+    }
+});
